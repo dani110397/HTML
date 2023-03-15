485ba1995ad0f373ea6f234f30f5d26a1a5287dd
Daniel <daniel.layer@biexcellence.com>
2023-03-15 11:10:30 +0100

    xxx

---
+++ BasicLayouts/ColappsibleSidbarLayout/index.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".collapse-sidebar").on("click",function(e){
+        e.preventDefault();
+        $(".sidebar-section").toggleClass("hidden");
+        $(".sidebar").toggleClass("show");
+    });
+
+    $(".slider").on("click",function(){
+        $(this).toggleClass("on");
+    });
+});
